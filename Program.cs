103ad1da73488798f3bd7b0c7fe41f5fe9e480bf
alfeudm <82668630+alfeudm@users.noreply.github.com>
2021-06-01 15:19:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace aula04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculadora simples
+
+            Inicio:
+            
+            Console.Clear();
+            
+            Console.Write("Digite um valor: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro valor: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Operação que deseja realizar: + - x /");
+            char op = char.Parse(Console.ReadLine());
+
+            double resultado;
+
+            switch (op)
+
+            {
+                default: 
+                    Console.WriteLine("Erro na operação");
+                 break;
+
+                case '+': 
+                    resultado = num1 + num2;
+                    Console.WriteLine("O resultado é: " + resultado);
+                break;
+
+                case '-':
+                    resultado = num1 - num2;
+                    Console.WriteLine("O resultado é: " + resultado);
+                break;
+
+                case 'x':
+                    resultado = num1 * num2;
+                    Console.WriteLine("O resultado é: " + resultado);
+                break;
+
+                case '/':
+                    
+                    if (num2 == 0)
+                        {
+                        Console.WriteLine("O número não pode ser dividido por 0");
+                        }
+                        else 
+                        {
+                            resultado = num1 / num2;
+                         Console.WriteLine("O resultado é: " + resultado);
+                        }
+                                         
+                break;
+            }
+
+            Console.WriteLine("Deseja continuar Calculando? [y/n]");
+            string opcao = Console.ReadLine();
+                if (opcao == "y" || opcao == "Y")
+                {    
+                goto Inicio;
+                }
+             Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
